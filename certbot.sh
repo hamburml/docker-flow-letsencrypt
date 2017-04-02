737c196f14ab69f67ac7dda50d36bc3cc05ec7c7
Jorian van Oostenbrugge <jovofdt@gmail.com>
2017-04-02 14:09:53 +0200

    Add a dry-run (with a retry mechanism) to certbot.sh (#18)
    * Update renewAndSendToProxy.sh
    
    Add a retry option in case curl fails
    
    * Update renewAndSendToProxy.sh
    
    * Update renewAndSendToProxy.sh
    
    Fix updating the wrong variable
    
    * Update renewAndSendToProxy.sh
    
    Move counter update to the end of the loop, so our counter logic is
    valid
    
    * Update renewAndSendToProxy.sh
    
    Add options to curl to hide the progress bar but show errors which can
    be helpful
    
    * Update renewAndSendToProxy.sh
    
    Move counter back to the beginning as this avoids any logic in the print
    statement
    
    * Update renewAndSendToProxy.sh
    
    Move sleep statement to avoid unnecessary sleep
    
    * Update certbot.sh
    
    - Add a dry-run (with a retry mechanism) to make sure Let's Encrypt is
    able to validate the domain, as in some environments this might take a
    while.
    - To avoid specifying the common arguments to certbot multiple times store them
    once.

--- certbot.sh
+++ certbot.sh
@@ -5,6 +5,12 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+#maximum number of retries
+MAXRETRIES=5
+
+#timeout
+TIMEOUT=5
+
 printf "${GREEN}Docker Flow: Let's Encrypt started${NC}\n";
 printf "We will use $CERTBOT_EMAIL for certificate registration with certbot. This e-mail is used by Let's Encrypt when you lose the account and want to get it back.\n";
 
@@ -13,6 +19,9 @@ if [ "$CERTBOTMODE" ]; then
   printf "${RED}Staging environment of Let's Encrypt is activated! The generated certificates won't be trusted. But you will not reach Let’s Encrypt's rate limits.${NC}\n";
   staging='--staging';
 fi
+
+#common arguments
+args=("--no-self-upgrade" "--standalone" "--non-interactive" "--expand" "--keep-until-expiring" "--email" "$CERTBOT_EMAIL" "--agree-tos" "$staging" "--preferred-challenges" "http-01" "--rsa-key-size" "4096" "--redirect" "--hsts" "--staple-ocsp")
 
 #we need to be careful and don't reach the rate limits of Let's Encrypt https://letsencrypt.org/docs/rate-limits/
 #Let's Encrypt has a certificates per registered domain (20 per week) and a names per certificate (100 subdomains) limit
@@ -30,16 +39,37 @@ until [  $COUNTER -lt 1 ]; do
   dom="";
   for i in "${arr[@]}"
   do
-    dom="$dom -d $i"
+    let exitcode=tries=0
+    until [ $tries -ge $MAXRETRIES ]
+    do
+      tries=$[$tries+1]
+      certbot-auto certonly --dry-run "${args[@]}" -d "$i" | grep -q 'The dry run was successful.' && break
+      exitcode=$?
+
+      if [ $tries -eq $MAXRETRIES ]; then
+        printf "${RED}Unable to verfiy domain ownership after ${tries} attempts.${NC}\n"
+      else
+        printf "${RED}Unable to verfiy domain ownership, we try again in ${TIMEOUT} seconds.${NC}\n"
+        sleep $TIMEOUT
+      fi
+    done
+
+    if [ $exitcode -eq 0 ]; then
+      printf "Domain $i successfully validated\n"
+      dom="$dom -d $i"
+    fi
   done
 
-  # check if DOMAINDIRECTORY exists, if it exists use --cert-name to prevent 0001 0002 0003 folders
-  if [ -d "$DOMAINDIRECTORY" ]; then
-    printf "\nUse certbot-auto certonly --cert-name ${arr[0]} --no-self-upgrade --standalone --non-interactive --expand --keep-until-expiring --email $CERTBOT_EMAIL $dom --agree-tos $staging --preferred-challenges http-01 --rsa-key-size 4096 --redirect --hsts --staple-ocsp $dom \n";
-    certbot-auto certonly --no-self-upgrade --cert-name ${arr[0]} --standalone --non-interactive --expand --keep-until-expiring --email $CERTBOT_EMAIL $dom --agree-tos $staging --preferred-challenges http-01 --rsa-key-size 4096 --redirect --hsts --staple-ocsp
-  else
-    printf "\nUse certbot-auto certonly --no-self-upgrade --standalone --non-interactive --expand --keep-until-expiring --email $CERTBOT_EMAIL $dom --agree-tos $staging --preferred-challenges http-01 --rsa-key-size 4096 --redirect --hsts --staple-ocsp $dom \n";
-    certbot-auto certonly --no-self-upgrade --standalone --non-interactive --expand --keep-until-expiring --email $CERTBOT_EMAIL $dom --agree-tos $staging --preferred-challenges http-01 --rsa-key-size 4096 --redirect --hsts --staple-ocsp
+  #only if we have successfully validated at least a single domain we have to continue
+  if [ -n "$dom" ]; then
+    # check if DOMAINDIRECTORY exists, if it exists use --cert-name to prevent 0001 0002 0003 folders
+    if [ -d "$DOMAINDIRECTORY" ]; then
+      printf "\nUse certbot-auto certonly %s --cert-name %s\n" "${args[*]}" "${arr[0]}";
+      certbot-auto certonly "${args[@]}" --cert-name "${arr[0]}" $dom
+    else
+      printf "\nUse certbot-auto certonly %s\n" "${args[*]}";
+      certbot-auto certonly "${args[@]}" $dom
+    fi
   fi
 
   
